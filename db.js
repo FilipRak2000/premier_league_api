4b54278897ee9b21b3f17948f36a1a0e9bf9b590
Filip <raczkins2000@interia.pl>
2022-10-12 19:02:41 +0200

    clear

--- db.js
+++ db.js
@@ -3,11 +3,11 @@ const Pool = require("pg").Pool
 
 
 const pool = new Pool({
-    user:  "postgres",
-    password: "test",
-    database: "premier_league",
-    host: "localhost",
-    port: 5432
+    user:  process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
 });
 
 module.exports = pool;
